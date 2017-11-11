6365b798eb8acdc2c0571643b33ac9f5c34a1fb9
FranRom <maildefranz@gmail.com>
2017-11-11 16:06:01 +0100

    Login, redirects, logout

--- routes/authController.js
+++ routes/authController.js
@@ -8,6 +8,10 @@ const User           = require("../models/user");
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
 
+authController.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
 authController.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
@@ -46,11 +50,61 @@ authController.post("/signup", (req, res, next) => {
           errorMessage: "Something went wrong when signing up"
         });
       } else {
-        // logged in, and now what
-        return;
+         res.redirect("/login");
       }
     });
   });
 });
+
+
+authController.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+authController.post("/login", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to log in"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username },
+    "_id username password following",
+    (err, user) => {
+      if (err || !user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      } else {
+        if (bcrypt.compareSync(password, user.password)) {
+          req.session.currentUser = user;
+           // res.redirect("/tweets");
+        } else {
+          res.render("auth/login", {
+            errorMessage: "Incorrect password"
+          });
+        }
+      }
+  });
+});
+
+
+authController.get("/logout", (req, res, next) => {
+  if (!req.session.currentUser) { res.redirect("/"); return; }
+
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/login");
+    }
+  });
+});
+
 
 module.exports = authController;
