eaa9d7ac4ad6780bed223ccc16622b6c4042ae2a
FranRom <maildefranz@gmail.com>
2017-11-11 15:51:05 +0100

    First Commit:Set-up and signup

---
+++ routes/authController.js
@@ -0,0 +1,56 @@
+const express        = require("express");
+const authController = express.Router();
+
+// User model
+const User           = require("../models/user");
+
+// Bcrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+
+authController.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authController.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(req.body);
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", {
+        errorMessage: "The username already exists"
+      });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", {
+          errorMessage: "Something went wrong when signing up"
+        });
+      } else {
+        // logged in, and now what
+        return;
+      }
+    });
+  });
+});
+
+module.exports = authController;
